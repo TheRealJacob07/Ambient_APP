4a7e970c10ed568dea4a98e5ee01fe87f1754a5c
TheRealJacob07 <jaseastrunk@gmail.com>
2023-11-07 11:04:00 -0600

    added mac and api key entrys

--- run.py
+++ run.py
@@ -3,4 +3,12 @@ import window
 import install
 install.install('aioambient')
 install.install('PySimpleGUI')
+apidata = open('api.txt', 'r+t')
+macdata = open('mac.txt', 'r+t')
+z = apidata.readline()
+y = macdata.readline()
+if z == 'api':
+    window.api_app()
+if y == 'mac':
+    window.mac_app()
 window.app()
